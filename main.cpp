5920db5bf54f69b215f73b2c3df5e910f863dd0f
polarecreater <hmst193@gmail.com>
2022-01-01 19:18:45 +0900

    2-3 부동소수점 형변환
    220101

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ using namespace std;
 
 int main() {
     //부동소수점
-    float a = 1.0;
+    float a = 1.0;//1.0은 원래 double인데 형변환됨
     double b = 1.0;
-    long double c = 1.0;
+    long double c = 1.0;//1.0은 원래 double인데 형변환됨
     
-    cout<<"float "<<sizeof(a)<<endl;//4
-    cout<<"double "<<sizeof(b)<<endl;//8
-    cout<<"long double "<<sizeof(c)<<endl;//16 - 맥이라서
+    cout<<"float "<<sizeof(1.0f)<<endl;//4
+    cout<<"double "<<sizeof(1.0)<<endl;//8
+    cout<<"long double "<<sizeof(1.0L)<<endl;//16 - 맥이라서
     
     return 0; //종료코드 0이면 정상
 }
