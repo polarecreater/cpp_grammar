a6206974124ae2646a8bb7da50dd81c2d5812162
polarecreater <hmst193@gmail.com>
2022-01-01 19:16:34 +0900

    2-3 부동소수점 float, double, long double
    220101

--- main.cpp
+++ main.cpp
@@ -12,16 +12,13 @@ using namespace std;
 
 int main() {
     //부동소수점
-    float a = 1.5;
-    float b = a*1.5;
-    float c = a/2;
-    float d = a-3;
+    float a = 1.0;
+    double b = 1.0;
+    long double c = 1.0;
     
-    cout<<"a "<<a<<endl;
-    cout<<"b "<<b<<endl;
-    cout<<"c "<<c<<endl;
-    cout<<"d "<<d<<endl;
-    //float 타입으로는 실수가 잘 표현됨
+    cout<<"float "<<sizeof(a)<<endl;//4
+    cout<<"double "<<sizeof(b)<<endl;//8
+    cout<<"long double "<<sizeof(c)<<endl;//16 - 맥이라서
     
     return 0; //종료코드 0이면 정상
 }
