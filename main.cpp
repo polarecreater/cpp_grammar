13b1f08ba9e52fbd6bb4e2106e61754df149694a
polarecreater <hmst193@gmail.com>
2022-01-01 18:08:36 +0900

    2-1 sizeof int, fixed size
    220101

--- main.cpp
+++ main.cpp
@@ -9,13 +9,24 @@
 using namespace std;
 
 int main() {
-    int num1 = 11;
-    int num2 = 011;//8진수
-    int num3 = 0b11;//2진수
-    int num4 = 0x11;//16진수
-    cout<<num1<<endl;
-    cout<<num2<< endl;//8+1
-    cout<<num3<<endl;//2+1
-    cout<<num4<<endl;//16+1
+    int intnum = 0;
+    long longnum = 0;
+    int* intPointer = &intnum;//포인터
+    
+    //64비트 맥 환경에서
+    cout<<sizeof(intnum)<<endl;//4
+    cout<<sizeof(longnum)<< endl;//8
+    cout<<sizeof(intPointer)<<endl;//8
+    
+    cout<<endl;
+    
+    //사이즈에 민감하다면 고정형 타입
+    int8_t a = 0;
+    int16_t b = 0;
+    int32_t c = 0;
+    
+    cout<< sizeof(a) <<endl;//1 //8비트라서 8로 나누면 1
+    cout<< sizeof(b) <<endl;//2
+    cout<< sizeof(c) <<endl;//4
     return 0; //종료코드 0이면 정상
 }
