09f110c435ddb80cd3bc4484bba8c028ea58f538
polarecreater <hmst193@gmail.com>
2022-01-01 17:47:04 +0900

    1-4 int
    220101

--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@
 //
 //  Created by 권혜민 on 2022/01/01.
 //
-
+using namespace std;
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    /*
+    int x,y,z; //선언 여러 개 가능
+    x = 1;
+    y = 2;
+    z = 3;
     std::cout << "Hello, World!\n";
-    return 0;
+    std::cout << x<<std::endl;//endl = 개행
+    */
+    //초기화 버전
+    int x(1), y(2), z=4;
+    
+    cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<z<<endl;
+    
+    return 0; //종료코드 0이면 정상
 }
