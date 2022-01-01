5f38cc75d21ee34d33ff11bce66e8a8c642d1be4
polarecreater <hmst193@gmail.com>
2022-01-01 16:59:56 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_grammar
+//
+//  Created by 권혜민 on 2022/01/01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
