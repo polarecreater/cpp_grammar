66828a74711a550e77d373344a8a3c7548f5558d
polarecreater <hmst193@gmail.com>
2022-01-02 22:09:53 +0900

    2-3 부동소수점 epsilon 구하기
    220102

--- main.cpp
+++ main.cpp
@@ -6,41 +6,32 @@
 //
 
 #include <iostream>
-#include <numeric>
+#include <cfloat>
 
 using namespace std;
 
 int main() {
     //부동소수점
-    float num0 = 0.1f;
-    float num1 = 0.02f * 5.0f;
-    cout.precision(64);//cout할 때 정밀하게 출력
-    cout<< num0 <<endl;
-    cout<< num1 <<endl;
-    
-    if(num0 == num1){//같은데 실행이 안 됨
-        cout << "Equal 1" <<endl;
-    }
-    if(num0 == 0.1f){
-        cout << "Equal 2" <<endl;//출력됨
-    }
-    if(num0 == 0.1){//0.1은 더블
-        cout << "Equal 3" <<endl;
-    }
-    if(num0 == 0.1L){//롱더블
-        cout << "Equal 4" <<endl;
-    }
-    cout<<0.1f<<endl;
-    cout<<0.1<<endl;
-    cout<<0.1L<<endl;
+    //1을 표현
+    //1 =  1.0 * 10^0(2)
+    unsigned int num0 = 0b00111111100000000000000000000000;//바이너리 형태로 표현(지수는 8자리, 0=127 / 가수는 23자리, 모두 0)
+    float num1;
+    memcpy(&num1,&num0,sizeof(num0));
+    cout.precision(64);
+    cout<<"num1 "<< num1 <<endl;//num1은 1으로 출력됨
     
     cout << endl;
     
-    //float은 ==으로 비교하면 위험함
-    //대신 차이의 절댓값과 epsilon(1보다 조금 큼)을 비교하는 경우도 있지만 항상 맞지 않음
-    if(fabsf(num0-num1) <= __FLT_EPSILON__){
-        cout<< "Equal 5" <<endl;
-    }
+    unsigned int num2 = 0b00111111100000000000000000000001;//1보다 크지만 가장 작은 수
+    float num3;
+    memcpy(&num3,&num2,sizeof(num2));
+    
+    cout<<"num3 "<< num3 <<endl; //1.00000011920928955078125
+    
+    //epsilon을 구하는 법
+    cout<< num3 - num1 << endl;
+    cout<< FLT_EPSILON << endl;
+    //둘이 정확히 같은 값
     
     return 0; //종료코드 0이면 정상
 }
