3351bb0583275ca85c1b4c2053ec6e4b493bfbd4
polarecreater <hmst193@gmail.com>
2022-01-02 21:59:14 +0900

    2-3 부동소수점 float, double, long double
    220102

--- main.cpp
+++ main.cpp
@@ -12,13 +12,36 @@ using namespace std;
 
 int main() {
     //부동소수점
-    unsigned int num0;
-    float num1 = -118.625f;
+    float num0 = 0.1f;
+    float num1 = 0.02f * 5.0f;
+    cout.precision(64);//cout할 때 정밀하게 출력
+    cout<< num0 <<endl;
+    cout<< num1 <<endl;
     
-    memcpy(&num0, &num1, sizeof(num1));//num1에 있는 값을 num0으로 카피
+    if(num0 == num1){//같은데 실행이 안 됨
+        cout << "Equal 1" <<endl;
+    }
+    if(num0 == 0.1f){
+        cout << "Equal 2" <<endl;//출력됨
+    }
+    if(num0 == 0.1){//0.1은 더블
+        cout << "Equal 3" <<endl;
+    }
+    if(num0 == 0.1L){//롱더블
+        cout << "Equal 4" <<endl;
+    }
+    cout<<0.1f<<endl;
+    cout<<0.1<<endl;
+    cout<<0.1L<<endl;
     
-    cout<<num0<<endl;//3270328320//num1의 메모리 레이아웃//이 값을 이진수로 변환하면
-    cout<<num1<<endl;//-118.625
+    cout << endl;
+    
+    //float은 ==으로 비교하면 위험함
+    //대신 차이의 절댓값과 epsilon(1보다 조금 큼)을 비교하는 경우도 있지만 항상 맞지 않음
+    if(fabsf(num0-num1) <= __FLT_EPSILON__){
+        cout<< "Equal 5" <<endl;
+    }
+    
     return 0; //종료코드 0이면 정상
 }
 
